d3a5c30c0849cfa071854a955bb10e6924310429
mikebru <michael.bruner3@gmail.com>
2019-01-04 10:42:55 -0600

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,9 @@ import logging
 import sys
 import time
 
+import cv2
+import numpy as np
+
 from tf_pose import common
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
